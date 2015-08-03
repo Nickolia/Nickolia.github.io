abac3cae7506dc8601221d23371b0d81d4c9cd3e
Xopxe <xopxem@gmail.com>
2015-08-04 00:46:59 +0300

    update

--- source/scripts/controllers/app-controller.js
+++ source/scripts/controllers/app-controller.js
@@ -2,6 +2,9 @@ app.controller('AppCtrl', ['$scope','$rootScope', '$mdSidenav','$location','json
     $scope.toggleSidenav = function(menuId) {
         $mdSidenav(menuId).toggle();
     };
+    $scope.toggleSidenavInIndex = function() {
+        $scope.show_left_nav = !$scope.show_left_nav
+    };
     $rootScope.page = {};
     $rootScope.page.index = $location.$$url == "/";
     $scope.posts = [];
